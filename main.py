f97815b5bb544b1554e6065a4c009084378c93f0
Lorenzo Olearo <lorenzo.olearo@olearo.com>
2023-06-01 17:40:17 +0200

    Implement comparison between DTC and FDCT

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from cosineimagecompression import CosineImageCompression
-from testDCT import TestCosineTransform
+from test.DCT import TestCosineTransform
+from test.performance import run_test
 from gui import GUI
 
 def main():
-    # TestCosineTransform().run_test()
+    TestCosineTransform().run_test()
     CosineImageCompression()
+    run_test()
     
 
 if __name__ == '__main__':
